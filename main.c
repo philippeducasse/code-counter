4f437a0a71c7468d33ffe77531c1548c50482548
ducasse@vdpresearch.de <philippe@NBPD082025.vdpGruppe.local>
2026-04-27 14:31:55 +0200

    added include flag

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
-
+#include <stdlib.h>
 struct LangStats
 {
     const char *ext;
@@ -32,7 +32,7 @@ int count_lines(const char *path)
     return count;
 }
 
-int should_skip_dir(const char *name)
+int should_skip_dir(const char *name, const char **included)
 {
     const char *skip[] = {".git",
                           "node_modules",
@@ -47,7 +47,14 @@ int should_skip_dir(const char *name)
     for (int i = 0; skip[i] != NULL; i++)
     {
         if (strcmp(name, skip[i]) == 0)
-            return 1;
+            for (int j = 0; included[j] != NULL; j++)
+            {
+                if (strcmp(name, included[j]) == 0)
+                {
+                    return 0;
+                }
+            }
+        return 1;
     }
     return 0;
 }
@@ -75,7 +82,7 @@ int is_counted_file(const char *name)
     return has_counted_extention(name, exts) || strstr(name, "ocker") != NULL;
 }
 
-int walk_dir(const char *path, struct LangStats stats[])
+int walk_dir(const char *path, struct LangStats stats[], const char **included)
 {
     DIR *dir = opendir(path);
 
@@ -91,7 +98,7 @@ int walk_dir(const char *path, struct LangStats stats[])
     {
         if (entry->d_name[0] == '.')
             continue; // skip hidden directories
-        if (should_skip_dir(entry->d_name))
+        if (should_skip_dir(entry->d_name, included))
             continue;
 
         char full_path[1024];
@@ -117,7 +124,7 @@ int walk_dir(const char *path, struct LangStats stats[])
         }
         else if (S_ISDIR(st.st_mode))
         {
-            walk_dir(full_path, stats);
+            walk_dir(full_path, stats, included);
         }
     }
 
@@ -146,7 +153,23 @@ int main(int argc, char *argv[])
 
     char *path = argv[1];
 
-    walk_dir(path, stats);
+    const char **included = malloc(argc * sizeof(const char *));
+
+    int included_count = 0;
+
+    if (argc > 2)
+    {
+        if (strcmp(argv[2], "--include") == 0)
+        {
+            for (int i = 3; i < argc; i++)
+            {
+                included[included_count] = argv[i];
+                included_count++;
+            }
+            included[included_count] = NULL;
+        }
+    }
+    walk_dir(path, stats, included);
 
     int total_lines = 0;
     int total_files = 0;
@@ -169,5 +192,6 @@ int main(int argc, char *argv[])
                    stats[i].file_count);
         }
     }
+    free(included);
     return 0;
 }
